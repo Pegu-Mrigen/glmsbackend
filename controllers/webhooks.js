68b81c6de8102f1a398c85aa6fe81ee5abec7b12
Pegu-Mrigen <mrig8520@gmail.com>
2025-04-15 15:14:02 +0530

    2d backend

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -21,8 +21,8 @@ export const clerkWebhooks = async (req, res) => {
     const { data, type } = req.body;
     //const { data } = req.body;
 
-    // switch (evt?.type) {
-    switch (type) {
+    switch (evt?.type) {
+    // switch (type) {
       case "user.created": {
         const userData = {
           _id: data.id,
